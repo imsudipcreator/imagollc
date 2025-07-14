e36912096260fbef934a7a0452e5c6ed07050b5f
imsudipcreator <imsudipmahata@gmail.com>
2025-07-14 14:26:50 +0530

    intelligence ui part 1

---
+++ src/app/intelligence/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IntelligencePage = () => {
+  return (
+    <div className='flex-1 bg-emerald-300'>
+      
+    </div>
+  )
+}
+
+export default IntelligencePage
