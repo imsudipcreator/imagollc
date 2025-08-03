9daedafa67fba3d67a1f0a50412997c17a2625a9
imsudipcreator <imsudipmahata@gmail.com>
2025-08-03 10:26:33 +0530

    some ui changes to home page

---
+++ public/assets/home/carousel/index.ts
@@ -0,0 +1,15 @@
+import KingFrog from "./king-frog.webp";
+import Mascot from "./mascot.webp";
+import NeonTokyo from "./neon-tokyo.webp";
+import PurpleBeetle from "./purple-beetle.webp";
+import SilverSurfer from "./silver-surfer.webp";
+import VintageBeetle from "./vintage-beetle.webp";
+
+export {
+  KingFrog,
+  Mascot,
+  NeonTokyo,
+  PurpleBeetle,
+  SilverSurfer,
+  VintageBeetle,
+};
